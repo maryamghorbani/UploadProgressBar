286311dc3f29ec8e720dda69c942be3c5d573c90
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-17 18:27:33 +0430

    add php file and css bootstrap CDN

--- index.php
+++ index.php
@@ -4,14 +4,16 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>U</title>
+    <title>Upload Progress Bar</title>
     <link href="https://cdn.rawgit.com/rastikerdar/vazir-font/v20.1.0/dist/font-face.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="css/style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+
 <!--    <link rel="stylesheet" href="./reset.css">-->
 </head>
 <body>
 
-    <header>UploadProgressBar</header>
+    <header>Upload Progress Bar</header>
     
     <script src="js/slider.js"></script>
     <script src="js/script.js"></script>
