6578372f71f0b01d218c57ad041e08a255af3202
Maryam <ghorbanimoghaddam.m@gmail.com>
2021-05-18 11:30:06 +0430

    add form and uplaad file

--- index.php
+++ index.php
@@ -7,13 +7,26 @@
     <title>Upload Progress Bar</title>
     <link href="https://cdn.rawgit.com/rastikerdar/vazir-font/v20.1.0/dist/font-face.css" rel="stylesheet" type="text/css" />
     <link rel="stylesheet" href="css/style.css">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet">
+
 
 <!--    <link rel="stylesheet" href="./reset.css">-->
 </head>
 <body>
 
-    <header>Upload Progress Bar</header>
+    <div class="container">
+        <div class="row">
+            <div class="col">
+                <form action="/upload.php" method="POST" enctype="multipart/form-data">
+                    <div class="form-group">
+                        <label for="fileupload"> File: </label>
+                        <input type="file" id="fileupload" name="file" class="form-control-file">
+                    </div>
+                    <button class="btn btn-danger">submit</button>
+                </form
+            </div>
+        </div>
+    </div>
     
     <script src="js/slider.js"></script>
     <script src="js/script.js"></script>
